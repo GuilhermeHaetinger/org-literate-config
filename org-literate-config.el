f9677e3e1058555f1c56143a0f88dccc5864d91f
Guilherme Haetinger <ghaetinger@gmail.com>
2020-10-21 16:57:34 -0300

    Added file existance error/tests

--- org-literate-config.el
+++ org-literate-config.el
@@ -18,7 +18,6 @@
 
 (require 'org)
 
-(defvar literate-config-ls)
 (defvar main-literate-config)
 
 (defun get-string-from-file (file-path)
@@ -31,10 +30,10 @@
   "Generate a MD5 hash value from the content under FILE-PATH."
   (md5 (get-string-from-file file-path)))
 
-(defun create-file-and-run (file-path func)
+(defun create-file-and-run (file args func)
   "Create file under FILE-PATH and run arbritary function FUNC."
-  (write-region "" nil file-path)
-  (funcall func file-path))
+  (write-region "" nil file)
+  (funcall func args))
 
 (defun was-there-change (file-path)
   "Check if there was any change on the file under FILE-PATH.
@@ -42,10 +41,10 @@ Do this by looking at the cached Hash value and the processed one.
 If there isn't such a file, create one and call was-there-change recursively."
   (let ((md5-file (concat (file-name-directory file-path)
                           (concat (file-name-base file-path) ".hash"))))
-  (if (file-exists-p file-path)
+  (if (and (file-exists-p file-path) (file-exists-p md5-file))
       (not (string= (get-string-from-file md5-file)
                     (md5-from-file file-path)))
-    (create-file-and-run file-path 'was-there-change))))
+    (create-file-and-run md5-file file-path 'was-there-change))))
 
 (defun save-and-load-lit-config (file-path)
   "Save (Tangle) the ORG file under FILE-PATH."
@@ -60,16 +59,18 @@ If there isn't such a file, create one and call was-there-change recursively."
   (interactive)
   (switch-to-buffer (find-file main-literate-config)))
 
-(defun run-configs ()
-  "Run the configs."
-  (let ((list literate-config-ls) (file nil))
+(defun run-configs (list)
+  "Run the configs defined in LIST."
+  (let ((file nil))
     (message "\n===== Checking Literate Config files =====")
     (while list
       (setq file (car list))
       (message (concat "\n=====[ ] " (concat file " =====\n")))
-      (if (was-there-change file)
-          (save-and-load-lit-config file))
-      (org-babel-load-file file)
+      (if (not (file-exists-p file))
+          (error "File doesn't exist!")
+        (if (was-there-change file)
+            (save-and-load-lit-config file))
+        (org-babel-load-file file))
       (setq list (cdr list))
       (message (concat "\n=====[X] " (concat file " =====\n"))))
     (message "== Checked All Config files! ==\n")))
--- tests/all-tests.el
+++ tests/all-tests.el
@@ -1,11 +1,23 @@
-(require 'org-literate-config)
+(load-file "../org-literate-config.el")
 
 ;;; Code:
 (ert-deftest test-string-from-file ()
   "Test if the function loads a file as string correctly."
-  (should (string= (get-string-from-file "res/sample.txt")
-                   "This is a sample text.")))
+  (should (string= (get-string-from-file "./res/sample.txt")
+                   "This is a sample text.
+")))
 
 (ert-deftest test-md5-from-file ()
-  (should (string= (md5-from-file "res/sample.txt")
+  (should (string= (md5-from-file "./res/sample.txt")
                    "a1fc87ae17cb8b1bce85cb6faa3bdf90")))
+
+(ert-deftest test-hash-creation ()
+  (delete-file "./res/exampleConfig.hash")
+  (was-there-change "./res/exampleConfig.org")
+  (should (file-exists-p "./res/exampleConfig.hash")))
+
+(ert-deftest run-empty-config ()
+  :expected-result :failed
+  (run-configs '("./res/nonexistent.org")))
+
+;;
